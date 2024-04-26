36135c3881d845f605f52872b96e46153a94de0e
jack-jackhui <jack_hui@msn.com>
2024-04-26 12:17:17 +1000

    updated shell script

--- run_youtube_uploader.sh
+++ run_youtube_uploader.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Change directory to the location of this script
+cd "$(dirname "$0")"
+
 # Set the environment variable
 export ENV="production"  # Change to "dev" for development environment
 
